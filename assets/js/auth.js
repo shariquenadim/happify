64f4c4296b4f9dfb1b62203e338a2cf602b14bd7
shariquenadim <66444722+shariquenadim@users.noreply.github.com>
2022-10-04 04:42:23 +0530

    links

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -8,28 +8,28 @@ function store(){
     var numbers = /[0-9]/g;
 
     if(name.value.length == 0){
-        alert('Please fill in name');
+        alert('Name is required.');
 
     }else if(mail.value.length == 0){
-        alert('Please fill in email');
+        alert('Email is mandetory.');
 
     }else if(pw.value.length == 0){
-        alert('Please fill in password');
+        alert('Please enter a valid password.');
 
     }else if(mail.value.length == 0 && pw.value.length == 0 && name.value.length == 0){
         alert('Please fill in name, email and password');
 
     }else if(pw.value.length > 8){
-        alert('Max of 8');
+        alert('Maximum length of password is 8');
 
     }else if(!pw.value.match(numbers)){
-        alert('please add 1 number');
+        alert('Password must contain at least one number');
 
     }else if(!pw.value.match(upperCaseLetters)){
-        alert('please add 1 uppercase letter');
+        alert('Password must contain at least one uppercase letter');
 
     }else if(!pw.value.match(lowerCaseLetters)){
-        alert('please add 1 lovercase letter');
+        alert('Password must contain at least one lowercase letter');
 
     }else{
         localStorage.setItem('name', name.value);
