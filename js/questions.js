48cc21d47f1204013427db70c45cda9f2b14e3ed
saurabh260120 <91410452+saurabh260120@users.noreply.github.com>
2022-10-05 22:21:23 +0530

    Assesment page added

---
+++ js/questions.js
@@ -0,0 +1,145 @@
+// creating an array and passing the number, questions, options, and answers
+
+let answers= [
+    "Did not apply to me at all",
+    "Applied to me to some degree, or some of the time",
+    "Applied to me to a considerable degree or a good part of time",
+    "Applied to me very much or most of the time"
+]
+let questions = [
+    {
+    numb: 1,
+    question: "I found it hard to wind down ",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "I was aware of dryness of my mouth",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "I couldn’t seem to experience any positive feeling at all ",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "I experienced breathing difficulty (e.g. excessively rapid breathing, breathlessness in the absence of physical exertion)",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "I found it difficult to work up the initiative to do things ",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 6,
+    question: "I tended to over-react to situations",
+  },
+    {
+    numb: 7,
+    question: "I experienced trembling (e.g. in the hands)",
+  },
+    {
+    numb: 8,
+    question: "I felt that I was using a lot of nervous energy",
+  },
+    {
+    numb: 9,
+    question: "I was worried about situations in which I might panic and make a fool of myself",
+  },
+    {
+    numb: 10,
+    question: "I felt that I had nothing to look forward to",
+  },
+    {
+    numb: 11,
+    question: "I found myself getting agitated",
+  },
+    {
+    numb: 12,
+    question: "I found it difficult to relax",
+  },
+    {
+    numb: 13,
+    question: "I felt down-hearted and blue",
+  },
+    {
+    numb: 14,
+    question: "I was intolerant of anything that kept me from getting on with what I was doing ",
+  },
+    {
+    numb: 15,
+    question: "I felt I was close to panic",
+  },
+    {
+    numb: 16,
+    question: "I was unable to become enthusiastic about anything ",
+  },
+    {
+    numb: 17,
+    question: "I felt I wasn’t worth much as a person ",
+  },
+    {
+    numb: 18,
+    question: "I felt that I was rather touchy ",
+  },
+    {
+    numb: 19,
+    question: "I was aware of the action of my heart in the absence of physical exertion (e.g. sense of heart rate increase, heart missing a beat)",
+  },
+    {
+    numb: 20,
+    question: "I felt scared without any good reason",
+  },
+    {
+    numb: 21,
+    question: "I felt that life was meaningless",
+  },
+  
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
